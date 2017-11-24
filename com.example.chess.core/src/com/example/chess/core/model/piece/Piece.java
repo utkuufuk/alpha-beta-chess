84a318d8b7d84300af4f71e5c6327e635595292b
utkuufuk <utkuufuk@gmail.com>
2017-11-25 00:17:46 +0300

    compute linear moves

--- com.example.chess.core/src/com/example/chess/core/model/piece/Piece.java
+++ com.example.chess.core/src/com/example/chess/core/model/piece/Piece.java
@@ -1,5 +1,6 @@
 package com.example.chess.core.model.piece;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.chess.core.model.Side;
@@ -37,5 +38,30 @@ public abstract class Piece
         {
             legalMoves.add(targetSquare);
         }
+    }
+
+    protected List<Square> computeLinearMoves(int horizontal, int vertical)
+    {
+        List<Square> legalMoves = new ArrayList<Square>();
+        Square targetSquare = square.getAdjacentSquare(horizontal, vertical);
+
+        while(targetSquare != null)
+        {
+            if (targetSquare.getPiece() == null)
+            {
+                legalMoves.add(targetSquare);
+            }
+            else if (targetSquare.getPiece().side == side)
+            {
+                break;
+            }
+            else
+            {
+                legalMoves.add(targetSquare);
+                break;
+            }
+            targetSquare = targetSquare.getAdjacentSquare(horizontal, vertical);
+        }
+        return legalMoves;
     }
 }
