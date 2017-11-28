34f80fd5d323a6a0b81e8af4c07cf1750a2ce07d
utkuufuk <utkuufuk@gmail.com>
2017-11-28 23:35:08 +0300

    create 8x8 matrix of squares in chess board part

--- com.example.chess.app/src/com/example/chess/app/parts/ChessBoardPart.java
+++ com.example.chess.app/src/com/example/chess/app/parts/ChessBoardPart.java
@@ -3,7 +3,11 @@ package com.example.chess.app.parts;
 import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 import com.example.chess.app.room.ChessRoom;
 import com.example.chess.core.model.Board;
@@ -11,13 +15,30 @@ import com.example.chess.core.model.Board;
 public class ChessBoardPart 
 {
     private ChessRoom chessRoom;
+    private Label[][] squares;
+
+    public ChessBoardPart()
+    {
+	    chessRoom = new ChessRoom();
+	    squares = new Label[Board.LENGTH][Board.LENGTH];
+    }
 
 	@PostConstruct
 	public void createComposite(Composite parent) 
 	{
-	    chessRoom = new ChessRoom();
-	    Board board = chessRoom.getBoard();
-	    board.display();
+	    parent.setLayout(new GridLayout(Board.LENGTH, true));
+	    GridData gridData = new GridData(SWT.CENTER, SWT.CENTER, false, false);
+
+	    for (int r = 0; r < Board.LENGTH; r++)
+	    {
+	        for (int c = 0; c < Board.LENGTH; c++)
+	        {
+	            squares[r][c] = new Label(parent, SWT.BORDER);
+	            squares[r][c].setLayoutData(gridData);
+                squares[r][c].setData(chessRoom.getBoard().getSquare(r, c));
+                squares[r][c].setText("Square");
+	        }
+	    }
 	}
 
 	@Focus
