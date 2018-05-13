345d87daceb1c65780ee78553e0728e7928d6c0d
utkuufuk <utkuufuk@gmail.com>
2018-05-14 00:27:54 +0300

    add row numbers & column letters to GUI

--- com.example.chess.app/src/com/example/chess/app/parts/ChessBoardPart.java
+++ com.example.chess.app/src/com/example/chess/app/parts/ChessBoardPart.java
@@ -14,30 +14,44 @@ import com.example.chess.core.model.Board;
 
 public class ChessBoardPart
 {
+    private static final String[] COL_LETTERS = {"A", "B", "C", "D", "E", "F", "G", "H"};
     private ChessRoom chessRoom;
     private Label[][] squares;
 
     public ChessBoardPart()
     {
         chessRoom = new ChessRoom();
-        squares = new Label[Board.LENGTH][Board.LENGTH];
+        squares = new Label[Board.LENGTH + 1][Board.LENGTH + 1];
     }
 
     @PostConstruct
     public void createComposite(Composite parent)
     {
-        parent.setLayout(new GridLayout(Board.LENGTH, true));
-        GridData gridData = new GridData(SWT.CENTER, SWT.CENTER, false, false);
+        parent.setLayout(new GridLayout(Board.LENGTH + 1, false));
+        GridData tagGridData = new GridData(SWT.CENTER, SWT.CENTER, true, false);
+        GridData squareGridData = new GridData(SWT.FILL, SWT.CENTER, true, false);
 
         for (int r = 0; r < Board.LENGTH; r++)
         {
+            squares[r][0] = new Label(parent, SWT.NONE);
+            squares[r][0].setLayoutData(tagGridData);
+            squares[r][0].setText(String.format("%s", r + 1));
+
             for (int c = 0; c < Board.LENGTH; c++)
             {
                 squares[r][c] = new Label(parent, SWT.BORDER);
-                squares[r][c].setLayoutData(gridData);
+                squares[r][c].setLayoutData(squareGridData);
                 squares[r][c].setData(chessRoom.getBoard().getSquare(r, c));
-                squares[r][c].setText(squares[r][c].getData().toString());
+                squares[r][c].setText(squares[r][c].getData().toString().substring(35));
             }
+        }
+        new Label(parent, SWT.NONE);
+        
+        for (int c = 0; c < Board.LENGTH; c++)
+        {
+            squares[Board.LENGTH][c + 1] = new Label(parent, SWT.NONE);
+            squares[Board.LENGTH][c + 1].setText(COL_LETTERS[c]);
+            squares[Board.LENGTH][c + 1].setLayoutData(tagGridData);
         }
     }
 
